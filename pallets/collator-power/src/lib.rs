9fec6c3e0f1a6800b296f2a793fdbce4107d5395
Konrad Stepniak <konrad@eiger.co>
2024-05-28 18:39:19 +0200

    feat: add storage maps for sps and collators

--- pallets/collator-power/src/lib.rs
+++ pallets/collator-power/src/lib.rs
@@ -44,26 +44,30 @@ pub mod pallet {
         type StoragePower: Parameter + Member + Clone + MaxEncodedLen;
 
         /// A stable ID for a Collator
-        type CollatorId: Parameter + Member + MaxEncodedLen;
+        type CollatorId: Parameter + Member + Ord + MaxEncodedLen;
 
         /// A stable ID for a Storage Provider
-        type StorageProviderId: Parameter + Member + MaxEncodedLen;
+        type StorageProviderId: Parameter + Member + Ord + MaxEncodedLen;
     }
 
     #[derive(
         Clone, Eq, PartialEq, Encode, Decode, RuntimeDebug, Default, TypeInfo, MaxEncodedLen,
     )]
-    pub struct StorageProviderClaim<StoragePower> {
-        /// Number of bytes stored by a miner
+    pub struct StorageProviderClaim<CollatorId: Ord, Balance, StoragePower> {
+        /// Number of bytes stored by a Storage Provider
         raw_bytes_power: StoragePower,
+        /// Stores how much currency was staked on a particular collator
+        delegates: BoundedBTreeMap<CollatorId, Balance, ConstU32<10>>
     }
 
     #[derive(
         Clone, Eq, PartialEq, Encode, Decode, Default, RuntimeDebug, TypeInfo, MaxEncodedLen,
     )]
-    pub struct CollatorClaim<Balance> {
+    pub struct CollatorClaim<Balance, StorageProviderId: Ord> {
         /// Amount of Currency that was pledged by a Collator to participate in block producer selection
-        pledged_collateral: Balance,
+        pledged_own_collateral: Balance,
+        /// Stores how much every Storage Provider has staked on a given Collator
+        delegated_collateral: BoundedBTreeMap<StorageProviderId, Balance, ConstU32<100>>
     }
 
     #[pallet::pallet]
@@ -72,12 +76,12 @@ pub mod pallet {
     #[pallet::storage]
     #[pallet::getter(fn collator_claims)]
     pub type CollatorClaims<T: Config> =
-        StorageMap<_, _, T::CollatorId, CollatorClaim<BalanceOf<T>>>;
+        StorageMap<_, _, T::CollatorId, CollatorClaim<BalanceOf<T>, T::StorageProviderId>>;
 
     #[pallet::storage]
     #[pallet::getter(fn storage_provider_claims)]
     pub type StorageProviderClaims<T: Config> =
-        StorageMap<_, _, T::StorageProviderId, StorageProviderClaim<T::StoragePower>>;
+        StorageMap<_, _, T::StorageProviderId, StorageProviderClaim<T::CollatorId, BalanceOf<T>, T::StoragePower>>;
 
     #[pallet::event]
     // #[pallet::generate_deposit(pub(super) fn deposit_event)]
@@ -94,10 +98,75 @@ pub mod pallet {
         StorageProviderAlreadyRegistered,
     }
 
+    /// Extrinsics exposed by the pallet
     #[pallet::call]
     impl<T: Config> Pallet<T> {
+        /// Registers account as a Storage Provider
+        /// Initially, Storage Provider has 0 Storage Power.
+        /// It needs to be added via UpdateStoragePower.
         pub fn register_storage_provider(_origin: OriginFor<T>) -> DispatchResultWithPostInfo {
             todo!()
+        }
+
+        /// After Storage Provider proved a sector, calls this method to update the bookkeeping about available power.
+        pub fn update_storage_power(
+            _storage_provider: OriginFor<T>,
+            _raw_delta_bytes: T::StoragePower,
+        ) -> DispatchResultWithPostInfo {
+            todo!()
+        }
+
+        /// Adds collator to the active collator set, the set is taken into account when `pallet collator selection` makes decision.
+        /// Collator initially has zero power.
+        /// Called with `root` privileges for now.
+        pub fn register_collator(_origin: OriginFor<T>) -> DispatchResultWithPostInfo {
+            todo!()
+        }
+
+        /// Sets a lock on `amount` of currency from a given `storageProvider`
+        /// Effectively staking `amount` on a `collator`.
+        /// Saves how much was staked, and later can be returned by `getActiveCollators()`
+        pub fn nominate_collator(
+            _storage_provider: OriginFor<T>,
+            _collator: T::CollatorId,
+            _amount: BalanceOf<T>,
+        ) -> DispatchResultWithPostInfo {
+            todo!()
+        }
+
+        /// Removes the `collator`'s nomination backed by `storageProvider`.
+        /// Returns pledged funds back to the `storageProvider`.
+        /// Remove a lock on `amount` of currency from a given `collator`.
+        /// Returns funds to the `storageProvider`.
+        pub fn denominate_collator(
+            _storage_provider: OriginFor<T>,
+            _collator: T::CollatorId,
+        ) -> DispatchResultWithPostInfo {
+            todo!()
+        }
+
+        /// Pledges a certain amount of balance for a more likelihood to be selected to be a block producer.
+        /// It's called by collator on its' own, as it's not always pledging it's entire balance.
+        pub fn pledge_collateral(_collator: OriginFor<T>, _amount: BalanceOf<T>) -> DispatchResultWithPostInfo {
+            todo!()
+        }
+    }
+
+    /// Functions exposed by the pallet
+    /// e.g. `pallet-collator-selection` used them to make decision about the next block producer
+    impl<T: Config> Pallet<T> {
+        /// Returns map of collator_id -> pledged collateral + its balance.
+        /// Essentially:
+        /// - adds pledged collateral from CollatorClaims
+        /// - goes through all of the Storage Provider 
+        pub fn active_collators() {
+            todo!();
+        }
+
+        /// Gets total power acquired by a collator
+        /// Total Power = ballance pledged by a collator and collateral staked on it by Storage Providers.
+        pub fn get_power(_collator: T::CollatorId) {
+            todo!();
         }
     }
 }
