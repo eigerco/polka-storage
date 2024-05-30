ab4a730614339c2ce44335a812e9b5251d5b2bfe
Rok Černič <rok.cernic@gmail.com>
2024-05-30 13:41:00 +0200

    fix: docs

--- storage/polka-index/src/piecestore/types.rs
+++ storage/polka-index/src/piecestore/types.rs
@@ -1,22 +1,22 @@
 use cid::Cid;
 use serde::{Deserialize, Serialize};
 
-/// PieceInfo is metadata about a piece a provider may be storing based on its
-/// piece_cid -- so that, given a piece_cid during retrieval, the miner can
-/// determine how to unseal it if needed
+/// Metadata about a piece that provider may be storing based on its [`Cid`]. So
+/// that, given a [`Cid`] during retrieval, the miner can determine how to
+/// unseal it if needed
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct PieceInfo {
     pub piece_cid: Cid,
     pub deals: Vec<DealInfo>,
 }
 
-/// DealId is an identifier for a retrieval deal (unique to a client)
+/// Identifier for a retrieval deal (unique to a client)
 type DealId = u64;
 
-/// SectorNumber is a numeric identifier for a sector. It is usually relative to a miner.
+/// Numeric identifier for a sector. It is usually relative to a miner.
 type SectorNumber = u64;
 
-/// DealInfo is information about a single deal for a given piece
+/// Information about a single deal for a given piece
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
 pub struct DealInfo {
     pub deal_id: DealId,
@@ -25,15 +25,14 @@ pub struct DealInfo {
     pub length: u64,
 }
 
-/// Contains information about where a given block is relative to the overall
-/// piece
+/// Information about where a given block is relative to the overall piece
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
 pub struct BlockLocation {
     pub rel_offset: u64,
     pub block_size: u64,
 }
 
-/// Contains block information along with the piece_id of the piece the block is
+/// Contains block information along with the [`Cid`] of the piece the block is
 /// inside of
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
 pub struct PieceBlockLocation {
@@ -41,7 +40,7 @@ pub struct PieceBlockLocation {
     pub location: BlockLocation,
 }
 
-/// CidInfo is information about where a given Cid will live inside a piece
+/// Information about where a given [`Cid`] will live inside a piece
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct CidInfo {
     pub cid: Cid,
