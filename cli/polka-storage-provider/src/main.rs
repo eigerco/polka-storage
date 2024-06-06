e948276fdc345a5f6325c8845e817b7e9a48f01a
Rok Černič <rok.cernic@gmail.com>
2024-06-06 10:40:23 +0200

    feat: use EnvFilter

--- cli/polka-storage-provider/src/main.rs
+++ cli/polka-storage-provider/src/main.rs
@@ -10,11 +10,15 @@ pub(crate) mod commands;
 pub(crate) use cli::Cli;
 use cli_primitives::Result;
 use commands::runner;
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     // Logger initialization.
-    tracing_subscriber::fmt().init();
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
 
     // Run requested command.
     runner::run().await
