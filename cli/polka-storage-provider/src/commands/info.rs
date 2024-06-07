82f55d38f70cf81cbc9b9e158e282fed45306198
Rok Černič <rok.cernic@gmail.com>
2024-06-07 17:52:59 +0200

    chore: small change to the commit

--- cli/polka-storage-provider/src/commands/info.rs
+++ cli/polka-storage-provider/src/commands/info.rs
@@ -8,7 +8,7 @@ pub(crate) struct InfoCommand {}
 impl InfoCommand {
     pub async fn handle(&self) -> Result<()> {
         // TODO(@cernicc,31/05/2024): Print start time of the provider
-        // TODO(@cernicc,07/06/2024): Print address used by the provider
+        // TODO(@cernicc,07/06/2024): Print polkadot address used by the provider
         unimplemented!()
     }
 }
