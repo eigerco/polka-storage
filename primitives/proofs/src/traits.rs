8c86cdb50e91e1b385c741db692ef15c018ee132
Konrad Stepniak <konrad@eiger.co>
2024-06-27 17:32:25 +0200

    docs(primitives-proofs): add docs for SectorDeal

--- primitives/proofs/src/traits.rs
+++ primitives/proofs/src/traits.rs
@@ -15,10 +15,21 @@ pub trait Market<AccountId, BlockNumber> {
 }
 
 /// Binds given Sector with the Deals that it should contain
+/// It's used as a data transfer object for extrinsics `verify_deals_for_activation`
+/// as well as `activate deals`.
+/// It represents a sector that should be activated and it's deals.
 #[derive(RuntimeDebug)]
 pub struct SectorDeal<BlockNumber> {
+    /// Number of the sector that is supposed to contain the deals
     pub sector_number: SectorNumber,
+    /// Time when the sector expires.
+    /// If sector expires before some of the deals end, than it's violation and sector is rejected.
     pub sector_expiry: BlockNumber,
+    /// Used to extract the size of a sector
+    /// All of the deals must fit within the seal proof's sector size.
+    /// If not, sector is rejected.
     pub sector_type: RegisteredSealProof,
+    /// Deals Ids that are supposed to be activated.
+    /// If any of those is invalid, whole activation is rejected.
     pub deal_ids: BoundedVec<DealId, ConstU32<128>>,
 }
