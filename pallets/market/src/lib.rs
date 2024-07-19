2efc191818512ff6200b2aa55d71a0523c6cd3c5
Rok Černič <rok.cernic@gmail.com>
2024-07-19 11:57:12 +0200

    chore: add some logs

--- pallets/market/src/lib.rs
+++ pallets/market/src/lib.rs
@@ -923,25 +923,27 @@ pub mod pallet {
             >,
             provider: &T::AccountId,
         ) -> Result<(), ProposalError> {
-            Self::validate_signature(
-                &Encode::encode(&deal.proposal),
-                &deal.client_signature,
-                &deal.proposal.client,
-            )?;
+            log::trace!(target: LOG_TARGET, "validating signature... {:?}", deal.client_signature);
+            let encoded = Encode::encode(&deal.proposal);
+            Self::validate_signature(&encoded, &deal.client_signature, &deal.proposal.client)?;
 
             // Ensure the Piece's Cid is parsable and valid
+            log::trace!(target: LOG_TARGET, "validating cid...");
             let _ = deal.proposal.cid()?;
 
+            log::trace!(target: LOG_TARGET, "checking if provider is the same across deals");
             ensure!(
                 deal.proposal.provider == *provider,
                 ProposalError::DifferentProvider
             );
 
+            log::trace!(target: LOG_TARGET, "check if deal.start_block < deal.end_block");
             ensure!(
                 deal.proposal.start_block < deal.proposal.end_block,
                 ProposalError::DealEndBeforeStart
             );
 
+            log::trace!(target: LOG_TARGET, "check if deal.state == PUBLISHED");
             ensure!(
                 deal.proposal.state == DealState::Published,
                 ProposalError::DealNotPublished
@@ -949,6 +951,7 @@ pub mod pallet {
 
             let min_dur = T::BlocksPerDay::get() * T::MinDealDuration::get();
             let max_dur = T::BlocksPerDay::get() * T::MaxDealDuration::get();
+            log::trace!(target: LOG_TARGET, "checking if deal.duration() {:?} <= {:?} <= {:?}", min_dur, deal.proposal.duration(), max_dur);
             ensure!(
                 deal.proposal.duration() >= min_dur && deal.proposal.duration() <= max_dur,
                 ProposalError::DealDurationOutOfBounds
