bb746b6e10afa86dc4e4ece09a4c73ad8d590d4d
José Duarte <duarte.gmj@gmail.com>
2024-07-23 13:16:24 +0000

    fix(storagext): deserialization tests

--- cli/polka-storage/storagext-cli/src/main.rs
+++ cli/polka-storage/storagext-cli/src/main.rs
@@ -1,14 +1,17 @@
 #![deny(clippy::unwrap_used)]
 
 mod cmd;
+mod deser;
 
 use std::fmt::Debug;
 
-use cid::Cid;
 use clap::{ArgGroup, Parser, Subcommand};
 use cmd::market::MarketCommand;
-use storagext::{BlockNumber, Currency, PolkaStorageConfig};
-use subxt::ext::sp_core::crypto::Ss58Codec;
+use deser::{DealProposal, DebugPair};
+use storagext::PolkaStorageConfig;
+use subxt::ext::sp_core::{
+    ecdsa::Pair as ECDSAPair, ed25519::Pair as Ed25519Pair, sr25519::Pair as Sr25519Pair,
+};
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::{
     filter, fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Layer,
@@ -32,20 +35,20 @@ struct Cli {
     /// Sr25519 keypair, encoded as hex, BIP-39 or a dev phrase like `//Alice`.
     ///
     /// See `sp_core::crypto::Pair::from_string_with_seed` for more information.
-    #[arg(long, value_parser = DebugPair::<subxt::ext::sp_core::sr25519::Pair>::value_parser)]
-    pub sr25519_key: Option<DebugPair<subxt::ext::sp_core::sr25519::Pair>>,
+    #[arg(long, value_parser = DebugPair::<Sr25519Pair>::value_parser)]
+    pub sr25519_key: Option<DebugPair<Sr25519Pair>>,
 
     /// ECDSA keypair, encoded as hex, BIP-39 or a dev phrase like `//Alice`.
     ///
     /// See `sp_core::crypto::Pair::from_string_with_seed` for more information.
-    #[arg(long, value_parser = DebugPair::<subxt::ext::sp_core::ecdsa::Pair>::value_parser)]
-    pub ecdsa_key: Option<DebugPair<subxt::ext::sp_core::ecdsa::Pair>>,
+    #[arg(long, value_parser = DebugPair::<ECDSAPair>::value_parser)]
+    pub ecdsa_key: Option<DebugPair<ECDSAPair>>,
 
     /// Ed25519 keypair, encoded as hex, BIP-39 or a dev phrase like `//Alice`.
     ///
     /// See `sp_core::crypto::Pair::from_string_with_seed` for more information.
-    #[arg(long, value_parser = DebugPair::<subxt::ext::sp_core::ed25519::Pair>::value_parser)]
-    pub ed25519_key: Option<DebugPair<subxt::ext::sp_core::ed25519::Pair>>,
+    #[arg(long, value_parser = DebugPair::<Ed25519Pair>::value_parser)]
+    pub ed25519_key: Option<DebugPair<Ed25519Pair>>,
 }
 
 #[derive(Debug, Subcommand)]
@@ -133,85 +136,4 @@ async fn main() -> Result<(), anyhow::Error> {
     }
 
     Ok(())
-}
-
-#[derive(Clone)]
-struct DebugPair<Pair>(Pair)
-where
-    Pair: subxt::ext::sp_core::Pair;
-
-impl<Pair> Debug for DebugPair<Pair>
-where
-    Pair: subxt::ext::sp_core::Pair,
-{
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_tuple("DebugPair")
-            .field(&self.0.public().to_ss58check())
-            .finish()
-    }
-}
-
-impl<Pair> DebugPair<Pair>
-where
-    Pair: subxt::ext::sp_core::Pair,
-{
-    fn value_parser(src: &str) -> Result<Self, String> {
-        Ok(Self(
-            Pair::from_string(&src, None).map_err(|err| format!("{}", err))?,
-        ))
-    }
-}
-
-/// CID doesn't deserialize from a string, hence we need our work wrapper.
-///
-/// <https://github.com/multiformats/rust-cid/issues/162>
-#[derive(Debug, Clone)]
-pub struct CidWrapper(Cid);
-
-// The CID has some issues that require a workaround for strings.
-// For more details, see: <https://github.com/multiformats/rust-cid/issues/162>
-impl<'de> serde::de::Deserialize<'de> for CidWrapper {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        let s = String::deserialize(deserializer)?;
-        let cid = Cid::try_from(s.as_str()).map_err(|e| {
-            serde::de::Error::custom(format!(
-                "failed to parse CID, check that the input is a valid CID: {e:?}"
-            ))
-        })?
-        Ok(Self(cid))
-    }
-}
-
-#[derive(Debug, Clone, serde::Deserialize)]
-pub struct DealProposal {
-    pub piece_cid: CidWrapper,
-    pub piece_size: u64,
-    pub client: <PolkaStorageConfig as subxt::Config>::AccountId,
-    pub provider: <PolkaStorageConfig as subxt::Config>::AccountId,
-    pub label: String,
-    pub start_block: BlockNumber,
-    pub end_block: BlockNumber,
-    pub storage_price_per_block: Currency,
-    pub provider_collateral: Currency,
-    pub state: storagext::runtime::runtime_types::pallet_market::pallet::DealState<BlockNumber>,
-}
-
-impl Into<storagext::DealProposal> for DealProposal {
-    fn into(self) -> storagext::DealProposal {
-        storagext::DealProposal {
-            piece_cid: self.piece_cid.0,
-            piece_size: self.piece_size,
-            client: self.client,
-            provider: self.provider,
-            label: self.label,
-            start_block: self.start_block,
-            end_block: self.end_block,
-            storage_price_per_block: self.storage_price_per_block,
-            provider_collateral: self.provider_collateral,
-            state: self.state.into(),
-        }
-    }
 }
