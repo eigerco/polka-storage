e39c62d8bca3aa50f1e8ee50956a115e80a69573
José Duarte <duarte.gmj@gmail.com>
2024-08-16 16:24:38 +0000

    docs: declare faults guide

--- examples/declare-fault.sh
+++ examples/declare-fault.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -xe
 
+# NOTE: we need to remove the target/debug
+
 # Export logging level and the storage provider key for simpler scripting
 export RUST_LOG=trace
 export SR25519_KEY="//Charlie"
@@ -8,7 +10,7 @@ export SR25519_KEY="//Charlie"
 # We'll do that for Alice (our client) and for Charlie (our storage provider):
 
 # Alice, using an explicit key
-target/debug/storagext-cli --sr25519-key //Alice market add-balance 25100200300
+target/debug/storagext-cli --sr25519-key "//Alice" market add-balance 25100200300
 
 # Charlie, with the implicit key, read from the SR25519_KEY environment variable
 target/debug/storagext-cli market add-balance 25100200300
@@ -18,59 +20,19 @@ target/debug/storagext-cli market add-balance 25100200300
 target/debug/storagext-cli storage-provider register charlie
 
 # We then register the deal between Alice and Charlie.
-#
-# We're using Heredoc to keep everything in the current script,
-# however, you can (and should) use a file path prepended with an @ symbol,
-# the expected input is the same and you can see the file in examples/deals.json.
-target/debug/storagext-cli --client-sr25519-key //Alice market publish-storage-deals << EOF
-[
-    {
-        "piece_cid": "bafk2bzacecg3xxc4f2ql2hreiuy767u6r72ekdz54k7luieknboaakhft5rgk",
-        "piece_size": 1,
-        "client": "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY",
-        "provider": "5FLSigC9HGRKVhB9FiEo4Y3koPsNmBmLJbpXg2mp1hXcS59Y",
-        "label": "dead",
-        "start_block": 30,
-        "end_block": 55,
-        "storage_price_per_block": 1,
-        "provider_collateral": 1,
-        "state": "Published"
-    }
-]
-EOF
+target/debug/storagext-cli market publish-storage-deals --client-sr25519-key "//Alice" "@examples/deals.json"
 
 # The provider now needs to pre-commit the received data,
 # if in 100 blocks (the `expiration` field) this data isn't proven,
 # the storage provider will receive a penalty (get his funds slashed).
-target/debug/storagext-cli storage-provider pre-commit << EOF
-{
-    "sector_number": 1,
-    "sealed_cid": "bafk2bzaceajreoxfdcpdvitpvxm7vkpvcimlob5ejebqgqidjkz4qoug4q6zu",
-    "deal_ids": [0],
-    "expiration": 100,
-    "unsealed_cid": "bafk2bzaceajreoxfdcpdvitpvxm7vkpvcimlob5ejebqgqidjkz4qoug4q6zu",
-    "seal_proof": "StackedDRG2KiBV1P1"
-}
-EOF
+target/debug/storagext-cli storage-provider pre-commit "@examples/pre-commit-sector.json"
 
 # Prove that we've properly stored the client's data.
-target/debug/storagext-cli storage-provider prove-commit << EOF
-{
-    "sector_number": 1,
-    "proof": "1230deadbeef"
-}
-EOF
+target/debug/storagext-cli storage-provider prove-commit "@examples/prove-commit-sector.json"
 
 # Let's now pretend that Charlie did an oopsie and the data the client trusted him has an issue,
 # to avoid getting an harsh penalty, Charlie needs to assume his mistake by declaring a fault:
-target/debug/storagext-cli storage-provider declare-faults << EOF
-[
-    {
-        "deadline": 0,
-        "partition": 0,
-        "sectors": [
-            1
-        ]
-    }
-]
-EOF
+target/debug/storagext-cli storage-provider declare-faults "@examples/fault-declaration.json"
+
+# In the meantime, Charlie undid his oopsie and can now say the sector is good for usage again:
+target/debug/storagext-cli storage-provider declare-faults-recovered "@examples/fault-declaration.json"
