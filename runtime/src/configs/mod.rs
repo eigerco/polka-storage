1b3345a674b8737453ac1fcbe466a5fdbd55c881
aidan <aidansullivan.dev@gmail.com>
2024-08-20 13:09:15 +0200

    fix: Adapt FaultDeclarationCutoff to substrate block time

--- runtime/src/configs/mod.rs
+++ runtime/src/configs/mod.rs
@@ -326,9 +326,9 @@ parameter_types! {
     pub const MaxPartitionsPerDeadline: u64 = 3000;
     pub const DeclarationsMax: u64 = 3000;
     pub const FaultMaxAge: BlockNumber = DAYS * 42;
-    /// Copied from FileCoin
+    /// Copied from FileCoin and adapted to substrate block time.
     /// <https://github.com/filecoin-project/builtin-actors/blob/6906288334746318385cfd53edd7ea33ef03919f/runtime/src/runtime/policy.rs#L327>
-    pub const FaultDeclarationCutoff: BlockNumber = (10 * MINUTES) + 50;
+    pub const FaultDeclarationCutoff: BlockNumber = (10 * MINUTES) + 125;
 
     // Market Pallet
     /// Deal duration values copied from FileCoin.
