3b1a1f5e919735d6269325081edaee28c12ebac2
Konrad Stepniak <konrad@eiger.co>
2024-09-19 13:15:25 +0200

    feat(pallet-proofs): port feistel network (#380)
    Co-authored-by: José Duarte <duarte.gmj@gmail.com>

--- pallets/proofs/src/porep/mod.rs
+++ pallets/proofs/src/porep/mod.rs
@@ -13,6 +13,7 @@ use crate::graphs::bucket::{BucketGraph, BucketGraphSeed, BASE_DEGREE};
 struct DomainSeparationTag(&'static str);
 
 const DRSAMPLE_DST: DomainSeparationTag = DomainSeparationTag("Filecoin_DRSample");
+const FEISTEL_DST: DomainSeparationTag = DomainSeparationTag("Filecoin_Feistel");
 
 /// Creates a seed for RNG, used for [`BucketGraph`] generation.
 /// References:
@@ -38,6 +39,24 @@ fn derive_porep_domain_seed(
         .into()
 }
 
+/// Feistel Cipher is used for [`StackedBucketGraph`] generation.
+/// Keys are derived deterministically based on `porep_id`.
+/// References:
+/// * <https://github.com/filecoin-project/rust-fil-proofs/blob/5a0523ae1ddb73b415ce2fa819367c7989aaf73f/storage-proofs-porep/src/stacked/vanilla/graph.rs#L80C1-L92C1>
+/// * <https://en.wikipedia.org/wiki/Feistel_cipher#Theoretical_work>
+pub fn derive_feistel_keys(porep_id: PoRepID) -> [u64; 4] {
+    let mut feistel_keys = [0u64; 4];
+    let raw_seed = derive_porep_domain_seed(FEISTEL_DST, porep_id);
+    feistel_keys[0] = u64::from_le_bytes(raw_seed[0..8].try_into().expect("seed to have 32 bytes"));
+    feistel_keys[1] =
+        u64::from_le_bytes(raw_seed[8..16].try_into().expect("seed to have 32 bytes"));
+    feistel_keys[2] =
+        u64::from_le_bytes(raw_seed[16..24].try_into().expect("seed to have 32 bytes"));
+    feistel_keys[3] =
+        u64::from_le_bytes(raw_seed[24..32].try_into().expect("seed to have 32 bytes"));
+    feistel_keys
+}
+
 pub struct ProofScheme;
 
 impl ProofScheme {
@@ -52,6 +71,8 @@ impl ProofScheme {
         // Just as showcase to ignore unused warnings for now.
         let mut parents = [0; BASE_DEGREE];
         drg.parents(0, &mut parents);
+
+        let _feistel_keys = derive_feistel_keys(config.porep_id());
 
         Self
     }
