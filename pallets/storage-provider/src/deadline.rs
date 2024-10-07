daaf884e254e089b0a889b4028d945d83f85a927
aidan <aidansullivan.dev@gmail.com>
2024-10-07 13:25:33 +0200

    docs: Add failure docs to deadline terminate_sectors

--- pallets/storage-provider/src/deadline.rs
+++ pallets/storage-provider/src/deadline.rs
@@ -333,6 +333,7 @@ where
     }
 
     /// Terminates sectors in the given partitions at the given block number
+    /// Fails if any of the partitions given in the `partition_sectors` is not found.
     /// 
     /// Reference implementation:
     /// * <https://github.com/filecoin-project/builtin-actors/blob/8d957d2901c0f2044417c268f0511324f591cb92/actors/miner/src/deadline_state.rs#L568>
