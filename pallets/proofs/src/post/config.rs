1792679b006193d097b5b208c2153bf929d0494e
Konrad Stepniak <konrad@eiger.co>
2024-10-21 17:06:10 +0200

    refactor: extract sector_count to primitives-proofs

--- pallets/proofs/src/post/config.rs
+++ pallets/proofs/src/post/config.rs
@@ -14,7 +14,7 @@ impl Config {
         Self {
             sector_size: post_type.sector_size().bytes(),
             challenges_per_sector: WINDOW_POST_CHALLENGE_COUNT,
-            challenged_sectors_per_partition: sector_count(post_type),
+            challenged_sectors_per_partition: post_type.sector_count(),
         }
     }
 }
@@ -24,14 +24,3 @@ impl Config {
 /// References:
 /// * <https://github.com/filecoin-project/rust-fil-proofs/blob/266acc39a3ebd6f3d28c6ee335d78e2b7cea06bc/filecoin-proofs/src/constants.rs#L32>
 const WINDOW_POST_CHALLENGE_COUNT: usize = 10;
-
-
-/// Number of sectors challenged in a replica.
-///
-/// References:
-/// * <https://github.com/filecoin-project/rust-fil-proofs/blob/266acc39a3ebd6f3d28c6ee335d78e2b7cea06bc/filecoin-proofs/src/constants.rs#L102>
-fn sector_count(post_type: RegisteredPoStProof) -> usize {
-    match post_type {
-        RegisteredPoStProof::StackedDRGWindow2KiBV1P1 => 2,
-    }
-}
