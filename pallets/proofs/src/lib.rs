1c578d2d0a2349062aacaeab759a4c65d791aada
aidan <aidansullivan.dev@gmail.com>
2024-10-30 13:04:53 +0100

    format: Fix cargo fmt issues

--- pallets/proofs/src/lib.rs
+++ pallets/proofs/src/lib.rs
@@ -28,8 +28,8 @@ pub mod pallet {
     use frame_system::pallet_prelude::*;
     use primitives_proofs::{
         ProofVerification, ProverId, PublicReplicaInfo, RawCommitment, RegisteredPoStProof,
-        RegisteredSealProof, SectorNumber, Ticket, MAX_SEAL_PROOF_BYTES, MAX_SECTORS_PER_PROOF,
-        MAX_POST_PROOF_BYTES,
+        RegisteredSealProof, SectorNumber, Ticket, MAX_POST_PROOF_BYTES, MAX_SEAL_PROOF_BYTES,
+        MAX_SECTORS_PER_PROOF,
     };
 
     use crate::{
