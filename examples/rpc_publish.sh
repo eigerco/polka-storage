ac92beda3835906073a46c081ecbc78dc610437d
th7nder <konrad@eiger.co>
2024-11-20 06:41:43 -0300

    fix: set post verifying key in rpc publish

--- examples/rpc_publish.sh
+++ examples/rpc_publish.sh
@@ -38,6 +38,7 @@ wait
 # Register the SP
 target/release/storagext-cli --sr25519-key "//Charlie" storage-provider register "peer_id"
 target/release/storagext-cli --sr25519-key "//Charlie" proofs set-porep-verifying-key @2KiB.porep.vk.scale
+target/release/storagext-cli --sr25519-key "//Charlie" proofs set-post-verifying-key @2KiB.post.vk.scale
 
 DEAL_JSON=$(
     jq -n \
