30e5fc4d2cef69b253e4b0c6e21e33b70433244c
aidan46 <aidansullivan.dev@gmail.com>
2025-01-15 16:27:26 +0800

    fix: Use debug for NodeType Display

--- storage-provider/server/src/p2p.rs
+++ storage-provider/server/src/p2p.rs
@@ -29,7 +29,7 @@ pub enum NodeType {
 
 impl Display for NodeType {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self)
+        std::fmt::Debug::fmt(&self, f)
     }
 }
 
