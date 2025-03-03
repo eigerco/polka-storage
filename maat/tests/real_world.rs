522ee348a4fb4523eceafd01bdff6d698f04b1cf
Konrad Stepniak <konrad@eiger.co>
2025-03-03 19:48:42 +0100

    change cwd

--- maat/tests/real_world.rs
+++ maat/tests/real_world.rs
@@ -192,8 +192,8 @@ async fn real_world_use_case() {
     setup_logging();
 
     let cwd = std::env::current_dir().unwrap();
-    println!("Current working directory: {:?}", cwd);
-    
+    tracing::info!("Current working directory: {:?}", cwd);
+
     let workspace_root = env::var("CARGO_MANIFEST_DIR").unwrap();
     let data_file_path = Path::new(&workspace_root)
         .join("..")
