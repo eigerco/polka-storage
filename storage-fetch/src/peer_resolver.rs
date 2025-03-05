a4de1511e92425c1f6b95878c2a072523d58f2fe
Rok Černič <rok.cernic@gmail.com>
2025-03-05 12:46:09 +0100

    peer_resolver refactor

--- storage-fetch/src/peer_resolver.rs
+++ storage-fetch/src/peer_resolver.rs
@@ -1,6 +1,6 @@
 use std::time::Duration;
 
-use anyhow::bail;
+use anyhow::{anyhow, bail};
 use libp2p::{
     futures::StreamExt,
     noise,
@@ -9,36 +9,65 @@ use libp2p::{
     tcp, yamux, Multiaddr, PeerId, StreamProtocol, Swarm, SwarmBuilder,
 };
 use primitives::p2p::{PeerIdRequest, PeerInfoResponse, BOOTSTRAP_REQUEST_RESPONSE_PROTOCOL};
-use tracing::{info, warn};
+use tracing::info;
 
+/// Get multiaddresses used by the peer.
 pub async fn find_multiaddr_storage_provider(
     bootstrap_address: Multiaddr,
     bootstrap_peer_id: PeerId,
     sp_peer_id: PeerId,
 ) -> Result<Vec<Multiaddr>, anyhow::Error> {
-    let swarm = create_swarm()?;
+    let mut swarm = create_swarm()?;
 
-    let peer_info = run_discover(swarm, bootstrap_address, &bootstrap_peer_id, sp_peer_id)
-        .await
-        .unwrap();
+    // Add known external peer to the swarm
+    swarm.add_peer_address(bootstrap_peer_id.clone(), bootstrap_address.clone());
+
+    // Request the info about the peer from the bootstrap node
+    swarm
+        .behaviour_mut()
+        .send_request(&bootstrap_peer_id, sp_peer_id.into());
+
+    // Wait for the response
+    let peer_info = wait_peer_info_response(swarm).await?;
 
     match peer_info {
-        PeerInfoResponse::NotFound(peer) => {
-            warn!(?peer, "peer is not registered");
-            bail!("{:?} is not registered", peer)
-        }
-        PeerInfoResponse::Found(peer_info) => {
-            info!(?peer_info, "received storage provider info");
-            Ok(peer_info.multiaddrs)
+        PeerInfoResponse::NotFound(peer) => Err(anyhow!("{:?} is not registered", peer)),
+        PeerInfoResponse::Found(peer_info) => Ok(peer_info.multiaddrs),
+    }
+}
+
+/// Pull the swarm until we receive peer info response or an error is observed.
+async fn wait_peer_info_response(
+    mut swarm: Swarm<request_response::cbor::Behaviour<PeerIdRequest, PeerInfoResponse>>,
+) -> Result<PeerInfoResponse, anyhow::Error> {
+    loop {
+        let Some(event) = swarm.next().await else {
+            bail!("Swarm was exhausted");
+        };
+
+        if let SwarmEvent::Behaviour(event) = event {
+            match event {
+                request_response::Event::Message { message, .. } => {
+                    if let Message::Response { response, .. } = message {
+                        info!(?response, "Received peer info response");
+                        return Ok(response);
+                    }
+                }
+                request_response::Event::OutboundFailure { error, .. } => {
+                    // This happens if we couldn't request the storage provider
+                    // info from the bootstrap node
+                    bail!(error)
+                }
+                _ => {}
+            }
         }
     }
 }
 
-/// Create a discovery swarm
 fn create_swarm(
 ) -> Result<Swarm<request_response::cbor::Behaviour<PeerIdRequest, PeerInfoResponse>>, anyhow::Error>
 {
-    let swarm = SwarmBuilder::with_new_identity()
+    Ok(SwarmBuilder::with_new_identity()
         .with_tokio()
         .with_tcp(
             tcp::Config::default(),
@@ -55,57 +84,5 @@ fn create_swarm(
             )
         })?
         .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(Duration::from_secs(10)))
-        .build();
-    Ok(swarm)
-}
-
-/// Run the discovery swarm and request the peer ID to multiaddrs mapping.
-async fn run_discover(
-    mut swarm: Swarm<request_response::cbor::Behaviour<PeerIdRequest, PeerInfoResponse>>,
-    bootstrap_addr: Multiaddr,
-    bootstrap_id: &PeerId,
-    resolve_id: PeerId,
-) -> Result<PeerInfoResponse, anyhow::Error> {
-    swarm.dial(bootstrap_addr)?;
-
-    loop {
-        tokio::select! {
-            event = swarm.select_next_some() => match event {
-                SwarmEvent::Behaviour(event) => match event {
-                    request_response::Event::Message { peer, message } => {
-                        if let Message::Response {
-                            request_id,
-                            response,
-                        } = message
-                        {
-                            tracing::info!("Received response with id {request_id} from {peer}");
-                            return Ok(response);
-                        }
-                    }
-                    request_response::Event::OutboundFailure {
-                        peer,
-                        request_id,
-                        error,
-                    } => {
-                        tracing::error!("Failed to send message with id {request_id} to {peer}: {error}");
-                        bail!("Failed to send message with id {request_id} to {peer}: {error}")
-                    }
-                    request_response::Event::InboundFailure {
-                        peer,
-                        request_id,
-                        error,
-                    } => {
-                        tracing::error!("Failed to receive message with id {request_id} from {peer}: {error}");
-                        bail!("Failed to receive message with id {request_id} from {peer}: {error}")
-                    }
-                    other => tracing::debug!("Unreachable event: {other:?}")
-                },
-                SwarmEvent::ConnectionEstablished { peer_id, .. } => {
-                    tracing::info!("Connected to {}", peer_id);
-                    swarm.behaviour_mut().send_request(bootstrap_id, resolve_id.into());
-                }
-                other => tracing::debug!("Received other event: {other:?}"),
-            }
-        }
-    }
+        .build())
 }
