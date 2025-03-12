794f58d6722525e5c522a3fc4ef4bfadbe27b4fa
José Duarte <duarte.gmj@gmail.com>
2025-03-12 16:40:03 +0000

    fix: clippy

--- node/src/cli.rs
+++ node/src/cli.rs
@@ -118,8 +118,8 @@ fn default_p2p_tcp_multiaddr() -> Multiaddr {
 }
 
 fn validate_tcp_multiaddr(s: &str) -> Result<Multiaddr, String> {
-    const IP4_TCP: [&'static str; 2] = ["ip4", "tcp"];
-    const IP6_TCP: [&'static str; 2] = ["ip6", "tcp"];
+    const IP4_TCP: [&str; 2] = ["ip4", "tcp"];
+    const IP6_TCP: [&str; 2] = ["ip6", "tcp"];
 
     let multiaddress = Multiaddr::from_str(s).map_err(|err| err.to_string())?;
     let protocols = multiaddress.protocol_stack().collect::<Vec<_>>();
@@ -148,7 +148,7 @@ pub struct RunCmd {
 
     /// Listen address in the P2P network of Storage Providers and Collators
     /// that the bootstrap node binds to.
-    #[arg(long, default_value_t=default_p2p_tcp_multiaddr(), value_parser = validate_tcp_multiaddr)]
+    #[arg(long, default_value_t = default_p2p_tcp_multiaddr(), value_parser = validate_tcp_multiaddr)]
     pub p2p_listen_address: Multiaddr,
 
     /// List of other bootstrap nodes
