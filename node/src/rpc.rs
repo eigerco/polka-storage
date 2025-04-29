81f1b2b9586b5220e2e230ebaff64c3e88fbec19
Konrad Stepniak <konrad@eiger.co>
2025-04-30 00:03:09 +0200

    fix: add to the response

--- node/src/rpc.rs
+++ node/src/rpc.rs
@@ -58,8 +58,17 @@ where
     module.merge(System::new(client.clone(), pool).into_rpc())?;
     module.merge(TransactionPayment::new(client).into_rpc())?;
     if let Some(config) = p2p {
+        let mut addresses = vec![];
+        if let Some(addr) = config.public_tcp_address {
+            addresses.push(addr);
+        }
+        if let Some(addr) = config.public_websocket_address {
+            addresses.push(addr);
+        }
+        addresses.push(config.tcp_address);
+        addresses.push(config.websocket_address);
         module.merge(
-            PolkaStorageServices::new(vec![config.tcp_address, config.websocket_address])
+            PolkaStorageServices::new(addresses)
                 .into_rpc(),
         )?;
     }
